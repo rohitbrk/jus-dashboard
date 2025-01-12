559166aa8c3e995a07bfd0a6c5a543222ec97eb6
Rohith <rohithkumarbathula99@gmail.com>
2025-01-12 23:55:47 +0530

    add home page and orders-list page using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import OrdersList from "./components/OrdersList/OrdersList";
 
 export default function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/orders-list" element={<OrdersList />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/OrdersList/OrdersList.jsx
+++ src/components/OrdersList/OrdersList.jsx
@@ -1,3 +1,3 @@
-export default function OrderList() {
+export default function OrdersList() {
   return <h1 className="text-3xl font-bold underline">OrderList</h1>;
 }
